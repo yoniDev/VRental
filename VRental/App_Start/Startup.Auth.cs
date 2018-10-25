53a0078a828e2deb29e07fa45824a32757255faa
yoniDev <yonasgindo@gmail.com>
2018-10-25 23:32:09 +0300

    Enabled facebook login

--- VRental/App_Start/Startup.Auth.cs
+++ VRental/App_Start/Startup.Auth.cs
@@ -54,9 +54,9 @@ namespace VRental
             //   consumerKey: "",
             //   consumerSecret: "");
 
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
+            app.UseFacebookAuthentication(
+               appId: "",
+               appSecret: "");
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
