80e0d4c00ac6fd66c28430d62694102a60608396
yoniDev <yonasgindo@gmail.com>
2018-11-10 18:41:20 +0000

    Add Rentals, Build an API for renting

---
+++ VRental/Controllers/Api/RentalsController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VRental.Dtos;
+using VRental.Models;
+
+namespace VRental.Controllers.Api
+{
+    public class RentalsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public RentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult CreateRentals(RentalDto rentalDto)
+        {
+            var customer = _context.Customers.Single(c => c.Id == rentalDto.CustomerId);
+            var movies = _context.Movies.Where(m => rentalDto.MovieIds.Contains(m.Id));
+
+            foreach (var movie in movies)
+            {
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ VRental/Dtos/RentalDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VRental.Models;
+
+namespace VRental.Dtos
+{
+    public class RentalDto
+    {
+        public int CustomerId { get; set; }
+        public List<int> MovieIds { get; set; }
+    }
+}
